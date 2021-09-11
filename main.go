abcaf2b2cbc77ba6b25774e7ffcb85f69c0594e2
Tamal Saha <tamal@appscode.com>
2021-09-11 09:21:33 -0700

    added-all

--- main.go
+++ main.go
@@ -1,4 +1,46 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"log"
+	"path/filepath"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func main() {
+	masterURL := ""
+	kubeconfigPath := filepath.Join(homedir.HomeDir(), ".kube", "config")
+
+	config, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfigPath)
+	if err != nil {
+		log.Fatalf("Could not get Kubernetes config: %s", err)
+	}
+
+	dc2 := dynamic.NewForConfigOrDie(config)
+
+	gvrNode := schema.GroupVersionResource{
+		Group:    "",
+		Version:  "v1",
+		Resource: "nodes",
+	}
+	nodes, err := dc2.Resource(gvrNode).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, obj := range nodes.Items {
+		fmt.Printf("%+v\n", obj.GetName())
+	}
+	nodes, err = dc2.Resource(gvrNode).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, obj := range nodes.Items {
+		fmt.Printf("%+v\n", obj.GetName())
+	}
+}
